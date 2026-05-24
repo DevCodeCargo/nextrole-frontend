c7f21444ab53106712c6ee6496e416730570979d
Nishant Badlani <nishantbadlani2@gmail.com>
2026-05-24 15:20:16 +0530

    added project setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layout/app-layout')
+        .then(m => m.AppLayout),
+    children: [
+      {
+        path: '',
+        redirectTo: 'register',
+        pathMatch: 'full'
+      },
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./modules/auth/pages/login')
+            .then(m => m.Login)
+      },
+      {
+        path: 'register',
+        loadComponent: () =>
+          import('./modules/auth/pages/register')
+            .then(m => m.Register)
+      }
+    ]
+  }
+];
---
+++ src/app/modules/auth/pages/register.ts
@@ -0,0 +1,54 @@
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService, LoginData, LoginRequest } from '../../../platform/services/auth.service';
+import { CommonModule } from '@angular/common';
+import { finalize, Observer } from 'rxjs';
+import { ApiResponse } from '../../../platform/models/api/api.model';
+
+@Component({
+  selector: 'nr-register',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './register.html'
+})
+export class Register {
+
+  model!: LoginRequest;
+
+  private authService = inject(AuthService);
+
+  errorMessage: string = "";
+  isLoading = signal(false);
+
+  constructor() {
+    this.model = {
+      username: "",
+      password: ""
+    }
+  }
+
+  onSubmit(): void {
+    this.errorMessage = '';
+    this.isLoading.set(true);
+
+    const observer: Partial<Observer<ApiResponse<LoginData>>> = {
+      next: (res) => {
+        console.log('Login success', res);
+        // later: store token + navigate
+      },
+      error: (err) => {
+        if (err.error?.message) {
+          this.errorMessage = err.error.message;
+        } else {
+          this.errorMessage = 'Something went wrong. Please try again.';
+        }
+      }
+    };
+
+    this.authService.login(this.model)
+      .pipe(
+        finalize(() => this.isLoading.set(false))
+      )
+      .subscribe(observer);
+  }
+
+}
